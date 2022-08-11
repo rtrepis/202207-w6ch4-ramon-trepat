952f624197cb8193be2e26caddb64c3b612f30dc
Ramon Trepat <r.trepis@gmail.com>
2022-08-11 19:04:15 +0200

    My first get

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import "./loadEnvironment";
 import Debug from "debug";
 import express from "express";
 import chalk from "chalk";
+import things from "./data/things";
 
 const debug = Debug("my-api:index");
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.json());
+
+app.get("/things", (req, res) => {
+  res.status(200).json(things);
+});
+
 app.listen(port, () => {
-  debug(chalk.blue(`Serve listening on http://localhost/:${port}`));
+  debug(chalk.blue(`Serve listening on http://localhost:${port}`));
 });
